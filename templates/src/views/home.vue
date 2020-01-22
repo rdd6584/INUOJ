a0f25a764850c2f9dd4ec45367313bcaec142fca
rdd6584 <rdd6584@naver.com>
2020-01-22 21:57:20 +0900

    123

---
+++ templates/src/views/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-row>
+        <v-layout>
+          <v-flex style="text-align:center;">
+            <h1>안녕하세요!</h1>
+            <h1>인천대학생 알고리즘 커뮤니티에 오신 것을 환영합니다.</h1>
+          </v-flex>
+        </v-layout>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
