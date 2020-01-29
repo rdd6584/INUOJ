43464a404fe98d9242a52256733ed79b83b5138f
orihehe <38060133+orihehe@users.noreply.github.com>
2020-01-30 03:41:30 +0900

    .

--- judgeServer/judge/publy.go
+++ judgeServer/judge/publy.go
@@ -1,9 +1,8 @@
 package judge
 
 const (
-	OurMysql      string = "rdd:20190325@tcp(192.168.0.42:3306)/inuoj"
+	OurMysql      string = "root:ojbydduck0325!@tcp(192.168.0.42:3306)/inuoj"
 	judgerDir     string = "../Judger"
-	hostDir       string = "/c/Users/rdd65/desktop/GoApp/src/INUOJ/Judger"
 	privDir       string = "../Judger/problems/private/"
 	pubDir        string = "../Judger/problems/public/"
 	submitDir     string = "../Judger/usercodes/"
